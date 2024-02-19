207997452ab132d6b06b408c6080cf53e1a4401d
Soojungkim <soojung.kimm@gmail.com>
2024-02-19 10:47:18 -0800

    one test failed but works as intended

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+
+  const [isInCart, setIsInCart] = useState(false);
+
+  const handleCartStatus = () => {
+    setIsInCart((prevStatus) => !prevStatus)
+  }
+
   return (
-    <li className="">
+    <li className={isInCart ? 'in-cart' : null}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleCartStatus} className="add">{isInCart ? 'Remove from cart' : 'Add to cart'}</button>
     </li>
   );
 }
